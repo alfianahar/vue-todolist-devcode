ad7f1a2424c4ade2a1837d1ed253ecbecc2e86c2
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-10-05 05:53:49 +0700

    fix try to remove teleport for testing

--- src/components/deleteModal.vue
+++ src/components/deleteModal.vue
@@ -1,55 +1,53 @@
 <template>
-  <Teleport to="body">
-    <Transition name="modal-outer">
-      <div
-        v-show="modalActive"
-        class="w-full bg-black bg-opacity-40 h-screen top-0 left-0 flex justify-center items-center fixed"
-        @click.self="toggleModal()"
-      >
-        <Transition name="modal-inner">
-          <div class="relative p-4 w-full max-w-xl h-auto md:h-auto">
-            <div class="relative bg-white rounded-lg shadow">
-              <form
-                autocomplete="off"
-                method="POST"
-                @submit.prevent="$emit('delete-modal')"
-              >
-                <div class="p-8 flex flex-col gap-7 items-center">
-                  <i
-                    class="fa-solid fa-triangle-exclamation fa-5x"
-                    style="color: #ed4c5c"
-                  ></i>
+  <Transition name="modal-outer">
+    <div
+      v-show="modalActive"
+      class="w-full bg-black bg-opacity-40 h-screen top-0 left-0 flex justify-center items-center fixed"
+      @click.self="toggleModal()"
+    >
+      <Transition name="modal-inner">
+        <div class="relative p-4 w-full max-w-xl h-auto md:h-auto">
+          <div class="relative bg-white rounded-lg shadow">
+            <form
+              autocomplete="off"
+              method="POST"
+              @submit.prevent="$emit('delete-modal')"
+            >
+              <div class="p-8 flex flex-col gap-7 items-center">
+                <i
+                  class="fa-solid fa-triangle-exclamation fa-5x"
+                  style="color: #ed4c5c"
+                ></i>
 
-                  <h1 class="text-center text-xl">
-                    Apakah anda yakin untuk menghapus
-                    <span class="font-bold">"{{ message }}"</span> ?
-                  </h1>
-                </div>
-                <!-- Modal footer -->
-                <div class="flex justify-center p-6 gap-4">
-                  <button
-                    class="bg-gray-200 text-black font-black py-3 px-12 rounded-full text-md inline-flex"
-                    type="button"
-                    @click="toggleModal()"
-                    data-cy="modal-delete-cancel-button"
-                  >
-                    Back
-                  </button>
-                  <button
-                    class="bg-[#ed4c5c] text-white font-bold py-3 px-12 rounded-full text-md inline-flex"
-                    type="submit"
-                    data-cy="modal-delete-confirm-button"
-                  >
-                    Hapus
-                  </button>
-                </div>
-              </form>
-            </div>
+                <h1 class="text-center text-xl">
+                  Apakah anda yakin untuk menghapus
+                  <span class="font-bold">"{{ message }}"</span> ?
+                </h1>
+              </div>
+              <!-- Modal footer -->
+              <div class="flex justify-center p-6 gap-4">
+                <button
+                  class="bg-gray-200 text-black font-black py-3 px-12 rounded-full text-md inline-flex"
+                  type="button"
+                  @click="toggleModal()"
+                  data-cy="modal-delete-cancel-button"
+                >
+                  Back
+                </button>
+                <button
+                  class="bg-[#ed4c5c] text-white font-bold py-3 px-12 rounded-full text-md inline-flex"
+                  type="submit"
+                  data-cy="modal-delete-confirm-button"
+                >
+                  Hapus
+                </button>
+              </div>
+            </form>
           </div>
-        </Transition>
-      </div>
-    </Transition>
-  </Teleport>
+        </div>
+      </Transition>
+    </div>
+  </Transition>
 </template>
 
 <script setup>
